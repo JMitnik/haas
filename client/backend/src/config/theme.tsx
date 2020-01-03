f29bc68a8e3b98293234eb80981ec45224de88b2
Jonathan Mitnik <jonathanmitnik@Jonathans-MacBook-Pro.local>
2020-01-03 19:14:31 +0100

    Better theme

--- client/backend/src/config/theme.tsx
+++ client/backend/src/config/theme.tsx
@@ -3,8 +3,21 @@ const theme = {
         primary: 'red',
         secondary: 'blue',
         tertiary: 'green',
+        black: '',
+        white: '',
     },
-    gutter: 24
+    gutter: 24,
+    // TODO: Add font
+    fontFamilies: {
+        body: '',
+        title: '',
+        special: '',
+    },
+    buttonSizes: {
+        sm: '12 24px',
+        md: '16 24px',
+        lg: '18 24px',
+    }
 };
 
 export default theme;
--- client/backend/src/views/AppContainer.tsx
+++ client/backend/src/views/AppContainer.tsx
@@ -1,6 +1,6 @@
 import React, { ReactChild } from 'react';
 import { Button } from '../components/UI/Buttons';
-import A
+
 const AppContainer = ({ children, ...props }: { children: ReactChild }) => {
     return (
         <div>
