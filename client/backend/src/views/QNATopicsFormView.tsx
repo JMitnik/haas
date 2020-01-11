7c8acb0ad52f0fac85c61001de02b71589cc25de
Cold-A-Muse <daan.998@hotmail.com>
2020-01-11 11:46:35 +0000

    Added initial question adding to QNA page

--- client/backend/src/views/QNATopicsFormView.tsx
+++ client/backend/src/views/QNATopicsFormView.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
@@ -12,15 +12,41 @@ const AddNewQuestionButton = styled.button`
     grid-column-end: 3;
 `;
 
+const CreateQuestionSection = styled.div`
+    grid-column-start: 1;
+    grid-column-end: 3;  
+    border: 1px solid black;
+  `
+;
+
+interface IQuestions {
+  question_title: string;
+}
+
 const QNATopicsForm = () => {
+  const ini: Array<IQuestions> = []
+  const [questions, setQuestions] = useState(ini)
+
   const { handleSubmit } = useForm();
   const onSubmit = (data: any) => { console.log(data); };
+
+  function handleNewQuestion(e: any) {
+    e.preventDefault();
+    console.log('Clicked button')
+    const questionNumber = questions.length + 1 
+    const title = `Question ${questionNumber.toString()}`
+    setQuestions([...questions, {question_title: title}])
+    
+  }
 
   return (
     <GridForm onSubmit={handleSubmit(onSubmit)}>
       <MiniHeader>QUESTIONS AND ANSWERS</MiniHeader>
       <InputSectionView full sub_title="TOPIC ALIAS NAME, USED AS IDENTIFIER" title="TOPIC NAME" />
-      <AddNewQuestionButton>Add new question</AddNewQuestionButton>
+      {questions?.map((question) => (
+                      <CreateQuestionSection>{question.question_title}</CreateQuestionSection>
+                    ))}
+      <AddNewQuestionButton  onClick={e => handleNewQuestion(e)}>Add new question</AddNewQuestionButton>
     </GridForm>
   );
 };
