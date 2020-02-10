3262d9d4f86d4ed1770e58b467df86eaa3ff80bf
Jonathan <JMitnik@gmail.com>
2020-02-10 23:40:05 +0100

    Clean 2

--- frontend/client/src/components/HAASSocialShare.tsx
+++ frontend/client/src/components/HAASSocialShare.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { H1, H5, Slider, Flex, ColumnFlex, Button, Div } from '@haas/ui';
 import { useFormContext } from 'react-hook-form';
-import { useJSONTree, MultiChoiceOption } from './hooks/use-json-tree';
+import { useJSONTree, MultiChoiceOption } from '../hooks/use-json-tree';
 import { Instagram } from 'react-feather';
 import { useTransition, animated } from 'react-spring';
 
