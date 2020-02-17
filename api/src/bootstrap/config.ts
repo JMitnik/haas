13da043ba00cd49edb2129888b6b429b87f8f201
Jonathan <JMitnik@gmail.com>
2020-02-17 22:25:00 +0100

    Gotem

--- api/src/bootstrap/config.ts
+++ api/src/bootstrap/config.ts
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const config: any = {
-    APP_PORT: process.env.APP_PORT || 4000,
+    APP_PORT: process.env.PORT,
     ENDPOINT: process.env.APP_ENDPOINT || '/graphql',
     PRISMA_ENDPOINT: process.env.PRISMA_ENDPOINT || 'http://localhost:4466',
     APP_SECRET: process.env.APP_SECRET,
@@ -9,5 +9,7 @@ const config: any = {
     PATH_TO_APP_SCHEMA: process.env.PATH_TO_APP_SCHEMA || './src/schema.graphql',
     PATH_TO_PRISMA_GENERATED_SCHEMA: process.env.PATH_TO_PRISMA_GENERATED_SCHEMA || './src/generated/prisma.graphql',
 };
+
+print(config);
 
 export default config;
