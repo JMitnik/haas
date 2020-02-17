7eec05fc4c148df8bfd3ec962f7da4c2151aadc8
Jonathan <JMitnik@gmail.com>
2020-02-17 22:38:53 +0100

    log2

--- api/src/bootstrap/config.ts
+++ api/src/bootstrap/config.ts
@@ -5,7 +5,6 @@ const config: any = {
     ENDPOINT: process.env.APP_ENDPOINT || '/graphql',
     PRISMA_ENDPOINT: process.env.PRISMA_ENDPOINT || 'http://localhost:4466',
     APP_SECRET: process.env.APP_SECRET,
-
     PATH_TO_APP_SCHEMA: process.env.PATH_TO_APP_SCHEMA || './src/schema.graphql',
     PATH_TO_PRISMA_GENERATED_SCHEMA: process.env.PATH_TO_PRISMA_GENERATED_SCHEMA || './src/generated/prisma.graphql',
 };
