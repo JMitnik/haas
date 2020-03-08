f60af72e368d236c9c53173a9b16aecd325489a1
Jonathan <JMitnik@gmail.com>
2020-03-08 12:55:25 +0100

    Whitespace

--- frontend/client/src/queries/getCustomerQuery.tsx
+++ frontend/client/src/queries/getCustomerQuery.tsx
@@ -7,5 +7,6 @@ export const getCustomerQuery = gql`
       ...CustomerFragment
     }
   }
+
   ${CustomerFragment}
 `;
--- frontend/client/src/queries/getQuestionnaireQuery.tsx
+++ frontend/client/src/queries/getQuestionnaireQuery.tsx
@@ -23,6 +23,7 @@ export const getQuestionnaireQuery = gql`
       }
     }
   }
+
   ${QuestionFragment}
   ${CustomerFragment}
 `;
