0ad87a699637c1e123b7af0c8de8f61d70f76093
Jonathan <jmitnik@gmail.com>
2020-03-15 14:04:54 +0100

    Config

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import makeApollo from './apollo';
 import config from './config';
 
 const main = async () => {
   const apollo = await makeApollo();
   const app = express();
-  apollo.applyMiddleware({ app });
-
+  apollo.applyMiddleware({
+    app,
+    cors: {
+      origin: [config.CLIENT_URL, config.DASHBOARD_URL],
+    },
+  });
+  app.use(cors());
   app.listen(config.APP_PORT);
 
   // App
