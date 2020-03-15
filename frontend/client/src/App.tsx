d7c1757a8bdc35fa1af92efc8e6ca1b75fc5eb51
Jonathan Mitnik <JMitnik@gmail.com>
2020-03-15 13:43:46 +0100

    Update App.tsx

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -14,6 +14,7 @@ const App: React.FC = () => {
   const { data, loading, error } = useQuery(getCustomerQuery);
   if (error) return <div>{'Error!' + error.message}</div>;
 
+  
   const customers = data?.customers;
 
   return (
