4be9ed9fd3f05bf1ec8830419a4b6ecac49807ce
Jonathan <jmitnik@gmail.com>
2020-03-15 15:14:01 +0100

    Fixed another random problem

--- api/src/app.ts
+++ api/src/app.ts
@@ -18,10 +18,9 @@ const main = async () => {
     const allowedOrigins = [config.CLIENT_URL, config.DASHBOARD_URL];
 
     const { origin } = req.headers;
+    console.log('The origin is: ', origin);
 
-    if (allowedOrigins.indexOf(origin) > -1) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
