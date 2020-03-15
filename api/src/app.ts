57c16c1b5c6aeaf62f429fe5e0db6fdb3fa0605f
Jonathan <jmitnik@gmail.com>
2020-03-15 14:22:24 +0100

    App

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,18 +6,19 @@ import config from './config';
 const main = async () => {
   const apollo = await makeApollo();
   const app = express();
+
   apollo.applyMiddleware({
     app,
     cors: {
+      credentials: true,
       origin: [config.CLIENT_URL, config.DASHBOARD_URL],
     },
   });
+
   app.use(cors());
   app.listen(config.APP_PORT);
 
-  // App
-
-  console.log(`Server successfully started on port ${config.APP_PORT}`);
+  console.log(`Server successfully started on port ${config.APP_PORT} for graphql entrypoint at ${apollo.graphqlPath}`);
 };
 
 main();
