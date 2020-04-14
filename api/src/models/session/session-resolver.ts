73400062c804b658433e70b8309f069fc319bffc
Jonathan <jmitnik@gmail.com>
2020-04-15 01:22:05 +0200

    Finished initial mail service

--- api/src/models/session/session-resolver.ts
+++ api/src/models/session/session-resolver.ts
@@ -1,12 +1,10 @@
-import { prisma } from '../../generated/prisma-client/index';
+import { prisma, NodeEntry } from '../../generated/prisma-client/index';
 import cleanInt from '../../utils/cleanInt';
-import { NodeEntryCreateWithoutSessionInput } from '../../generated/resolver-types';
+import { NodeEntryCreateWithoutSessionInput, Session } from '../../generated/resolver-types';
 
 class SessionResolver {
   static async uploadUserSession(obj: any, args: any, ctx: any) {
     const { questionnaireId, entries } = args.uploadUserSessionInput;
-
-    const nodeEntries = entries.map((entry: any) => SessionResolver.constructNodeEntry(entry));
 
     const session = await prisma.createSession({
       questionnaire: {
@@ -15,23 +13,42 @@ class SessionResolver {
         },
       },
       nodeEntries: {
-        create: nodeEntries,
+        create: entries.map((entry: any) => SessionResolver.constructNodeEntry(entry)),
       },
     });
 
     // TODO: Replace this with email associated to dialogue (or fallback to company)
     const dialogueAgentMail = 'jmitnik@gmail.com';
 
+    // TODO: Roundabout way, needs to be done in Prisma2 better
+    const nodeEntries = await SessionResolver.getEntriesOfSession(session);
+    const questionnaire = await prisma.questionnaire({ id: questionnaireId });
+
     ctx.services.triggerMailService.sendTrigger({
       to: dialogueAgentMail,
       userSession: {
         id: session.id,
         nodeEntries,
-        questionnaire: { id: questionnaireId },
+        questionnaire,
       },
     });
 
     return session;
+  }
+
+  static async getEntriesOfSession(session: Session): Promise<NodeEntry[]> {
+    const entries = await Promise.all((
+      await prisma.session({ id: session.id }).nodeEntries()).map(async (entry) => ({
+      ...entry,
+      relatedNode: await Promise.resolve(prisma.nodeEntry({
+        id: entry.id,
+      }).relatedNode()),
+      values: await Promise.resolve(prisma.nodeEntry({
+        id: entry.id,
+      }).values()),
+    })));
+
+    return entries;
   }
 
   static constructNodeEntry(nodeEntry: any) : NodeEntryCreateWithoutSessionInput {
--- api/src/services/mailings/trigger-mail-service.ts
+++ api/src/services/mailings/trigger-mail-service.ts
@@ -14,43 +14,75 @@ interface SendTriggerProps {
 
 class TriggerMailService extends MailService {
   sendTrigger({ from, to, userSession }: SendTriggerProps) {
-    // TODO: Put in MJML
-    const testArr = [2, 3, 4];
-
-    console.log('Sending mail!!!!');
-
     const mailBody = mjml2html(`
-      <mjml>
-        <mj-body background-color="#e0f2ff">
-          <mj-section background-color="#4ca9eb">
-              <mj-column>
-                  <mj-text font-size="20px" color="white" align="center">
-                      Dear ${to}
-                  </mj-text>
-              </mj-column>
-          </mj-section>
-          <mj-section background-color="white">
-              <mj-column width="100%">
-                  <mj-text>
-                      We notice one of your customers had something to say about dialogue #${userSession.questionnaire?.id}.
-                  </mj-text>
-              </mj-column>
-          </mj-section>
-
-          ${userSession?.nodeEntries?.map((entry) => `
-            <mj-section border-top="1px solid #ddd" background-color="white">
-                <mj-column width="100%">
-                    <mj-text>
-                        For node ${entry.relatedNode?.id}: ${entry?.values?.map((val) => `
-                          Value: ${val.numberValue || val.textValue})}
-                        `)}
+        <mjml>
+        <mj-body background-color="#0059f8">
+            <mj-section text-align="center">
+                <mj-column>
+                    <mj-image align="center" src="https://res.cloudinary.com/dx8khik9g/image/upload/v1586902351/haas/logo-haas.png" width="125px" alt="" />
+                </mj-column>
+            </mj-section>
+            <mj-spacer height="30px" />
+            <mj-section border-radius="5px 5px 0 0"  background-color="#6597fd">
+                <mj-column>
+                    <mj-text font-size="20px" color="white" align="left">
+                        Notification: A new trigger has been activated!
                     </mj-text>
                 </mj-column>
             </mj-section>
-          `)}
+            <mj-section background-color="#eef1f5">
+                <mj-column width="100%">
+                    <mj-text>
+                        Dear ${to},
+                        one of your set-up triggers has been activated.
 
+                        One of your customers had an opinion regarding dialogue ${userSession.questionnaire?.title},
+                        for session nr ${userSession.id}. You can find below details
+                        more detail about their dialogue
+                    </mj-text>
+                </mj-column>
+            </mj-section>
+            <mj-section background-color="white">
+                <mj-column>
+                    <mj-text align="center">They rated your dialogue with score</mj-text>
+                    <mj-text font-size="30px" align="center">${userSession?.nodeEntries?.[0].values?.[0].numberValue}</mj-text>
+                </mj-column>
+            </mj-section>
+
+            ${userSession.nodeEntries?.slice(1).map((entry) => `
+            <mj-section border-top="1px solid #ddd" background-color="white">
+                <mj-group>
+                    <mj-column width="50%">
+                        <mj-text>
+                            You asked them:
+                        </mj-text>
+                        <mj-text>
+                            ${entry.relatedNode?.title}
+                        </mj-text>
+                    </mj-column>
+                    <mj-column width="50%">
+                        <mj-text>
+                            They answered:
+                        </mj-text>
+                        <mj-text>
+                            ${entry.values?.map((val) => `
+                                ${val.textValue || val.numberValue || 'Not supported yet!'}
+                            `)}
+                        </mj-text>
+                    </mj-column>
+                </mj-group>
+            </mj-section>
+            `)}
+
+            <mj-section>
+                <mj-column>
+                    <mj-text align="center">
+                        <a class="mj-content" href="https://haas.live/unsubscribe">Unsubscribe from these triggers</a>
+                    </mj-text>
+                </mj-column>
+            </mj-section>+
         </mj-body>
-      </mjml>
+        </mjml>
     `).html;
 
     this.sendMail({
