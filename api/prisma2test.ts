01bf37109fd1442ea0c4b968ca2c7e30f267d00b
Cold-A-Muse <daan.998@hotmail.com>
2020-04-21 18:17:12 +0200

    First prisma2 migration for dialogue, question node and edges Co-authored-by: Jonathan Mitnik <JMitnik@gmail.com>

---
+++ api/prisma2test.ts
@@ -0,0 +1,78 @@
+import { PrismaClient } from '@prisma/client';
+// or const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+
+  const edge = await prisma.edge.create({
+    data: {
+      parentNode: {
+        create: {
+          title: 'parentNode',
+          type: 'SLIDER',
+        },
+      },
+      childNode: {
+        create: {
+          title: 'childNode',
+          type: 'multi-value',
+        },
+      },
+      conditions: {
+        create: [
+          {
+            conditionType: 'match',
+            matchValue: 'FACILITIES',
+          },
+        ],
+      },
+    },
+  });
+
+  const dialogue = await prisma.dialogue.create({
+    data: {
+      title: 'Dialogue',
+      description: 'Desc.',
+      questions: {
+        create: {
+          title: 'OverrideLeaf Question',
+          type: 'MULTI-VALUE',
+          options: {
+            create: {
+              value: 'Option 1',
+            },
+          },
+          overrideLeaf: {
+            create: {
+              title: 'Leaf',
+              type: 'Social-Share',
+            },
+          },
+          children: {
+            connect: {
+              id: edge.id,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  // console.log('Edge dialogue id: ', await prisma.dialogue.findOne({ where: { id: dialogue.id } }).edges());
+  // await prisma.questionNode.create({
+  //   data: {
+  //     title: 'Question1',
+  //     type: 'Multi-Value',
+  //   },
+  // });
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.disconnect();
+  });
