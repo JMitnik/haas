4a6b781f1ef4e1187584fa89f6dc008bc15fc87a
Cold-A-Muse <daan.998@hotmail.com>
2020-06-11 17:49:49 +0200

    Cleanup

--- frontend/dashboard/src/views/UsersOverview/TableRow/Row.tsx
+++ frontend/dashboard/src/views/UsersOverview/TableRow/Row.tsx
@@ -1,23 +1,15 @@
 import { Edit, X } from 'react-feather';
-import { useHistory, useParams } from 'react-router';
 import React, { useState } from 'react';
 
-import { DeleteButtonContainer, Div, EditButtonContainer, Grid, H4, H5, Hr, Span } from '@haas/ui';
+import { DeleteButtonContainer, EditButtonContainer, Grid } from '@haas/ui';
 import { UserRowProps } from 'components/Table/RowComponentInterfaces';
 
 const UserRow = ({ headers, data, index, onDeleteEntry, onEditEntry }: UserRowProps) => {
-  const history = useHistory();
-  const { customerId } = useParams<{ customerId: string }>();
   const [isExpanded, setIsExpanded] = useState(false);
   const amtCells = headers.length;
   const percentage = 100 / amtCells;
   const templateColumns = `${percentage.toString()}% `.repeat(amtCells);
   const userId = data.id;
-
-  const setEditUser = (event: any, userId: string) => {
-    history.push(`/dashboard/c/${customerId}/u/${userId}/edit`);
-    event.stopPropagation();
-  };
 
   return (
     <Grid style={{ position: 'relative' }} gridRowGap={0} gridColumnGap={5} gridTemplateColumns={templateColumns} onClick={() => setIsExpanded(!isExpanded)}>
