6702db5e78da418c68f30c8fb44b0cbb40b7e86b
Cold-A-Muse <daan.998@hotmail.com>
2020-07-31 14:01:42 +0200

    More modern date picker

--- frontend/dashboard/src/components/DatePicker/DatePicker.tsx
+++ frontend/dashboard/src/components/DatePicker/DatePicker.tsx
@@ -1,10 +1,10 @@
 import 'react-datepicker/dist/react-datepicker.css';
-import { XCircle } from 'react-feather';
-import React, { useState } from 'react';
+import { Calendar } from 'react-feather';
+import React from 'react';
 import ReactDatePicker from 'react-datepicker';
 
-import { Div } from '@haas/ui';
-import DatePickerFilter from 'components/DatePicker/DatePickerFilter';
+import { Div, Flex, Span } from '@haas/ui';
+import styled, { css } from 'styled-components';
 
 interface DatePickerProps {
   activeStartDate: Date | null;
@@ -12,42 +12,70 @@ interface DatePickerProps {
   onDateChange: (start: Date | null, end: Date | null) => void;
 }
 
-const DatePicker = ({ activeStartDate, activeEndDate, onDateChange }: DatePickerProps) => {
-  const [isActive, setIsActive] = useState(false);
+const CustomPickerInput = styled.input`
+  ${({ theme }) => css`
+    background: ${theme.colors.app.mutedOnDefault};
+    border: 1px solid ${theme.colors.default.dark};
+    width: 75px;
 
-  return (
-    <Div mr="5px">
-      {!isActive && (
-        <DatePickerFilter
-          activeStartDate={activeStartDate}
-          activeEndDate={activeEndDate}
-          setIsActive={setIsActive}
+    &:focus {
+      transition: all 0.2s ease-in;
+      outline-width: 0;
+      border: 1px solid ${theme.colors.app.mutedAltOnDefault};
+      box-shadow: 0 0 0 1px ${theme.colors.app.mutedAltOnDefault};
+    }
+  `};
+`;
+
+const DatePicker = ({ activeStartDate, activeEndDate, onDateChange }: DatePickerProps) => (
+  <Div mr="5px">
+    <Div padding="4.5px 12px" borderRadius="6px" useFlex flexDirection="row" backgroundColor="app.mutedOnDefault">
+      <Flex>
+        <Span fontWeight="200" color="app.mutedAltOnDefault">
+          Start
+        </Span>
+        <Calendar stroke="#838890" width="1em" style={{ marginLeft: '5px', marginRight: '5px' }} />
+        <ReactDatePicker
+          selected={activeStartDate}
+          onChange={(date) => date !== activeStartDate && onDateChange(date, activeEndDate)}
+          selectsStart
+          startDate={activeStartDate}
+          endDate={activeEndDate}
+          customInput={(
+            <CustomPickerInput
+              type="text"
+              id="CustomID"
+              placeholder="Date"
+            />
+            )}
         />
-      )}
-      {isActive && (
-        <Div padding={15} borderRadius="90px" useFlex flexDirection="row" backgroundColor="#f1f5f8">
-          <ReactDatePicker
-            selected={activeStartDate}
-            onChange={(date) => date !== activeStartDate && onDateChange(date, activeEndDate)}
-            selectsStart
-            isClearable
-            startDate={activeStartDate}
-            endDate={activeEndDate}
-          />
-          <ReactDatePicker
-            selected={activeEndDate}
-            onChange={(date) => date !== activeEndDate && onDateChange(activeStartDate, date)}
-            selectsEnd
-            isClearable
-            startDate={activeStartDate}
-            endDate={activeEndDate}
-            minDate={activeStartDate}
-          />
-          <XCircle onClick={() => setIsActive(false)} color="#6d767d" style={{ marginLeft: '10px' }} />
-        </Div>
-      )}
+      </Flex>
+      <Span padding="0 5px" color="app.mutedAltOnDefault">
+        |
+      </Span>
+      <Flex>
+        <Span fontWeight="200" color="app.mutedAltOnDefault">
+          End
+        </Span>
+        <Calendar stroke="#838890" width="1em" style={{ marginLeft: '5px', marginRight: '5px' }} />
+        <ReactDatePicker
+          selected={activeEndDate}
+          onChange={(date) => date !== activeEndDate && onDateChange(activeStartDate, date)}
+          selectsEnd
+          startDate={activeStartDate}
+          endDate={activeEndDate}
+          minDate={activeStartDate}
+          customInput={(
+            <CustomPickerInput
+              type="text"
+              id="CustomID"
+              placeholder="Date"
+            />
+            )}
+        />
+      </Flex>
     </Div>
-  );
-};
+  </Div>
+);
 
 export default DatePicker;
