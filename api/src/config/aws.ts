9e8d29290c0274d7f0d2e47d2ca763e809026fdc
Jonathan Mitnik <jonathanmitnik@Jonathans-MacBook-Pro.local>
2020-08-25 17:10:19 +0200

    Better indents

--- api/src/config/aws.ts
+++ api/src/config/aws.ts
@@ -1,15 +1,15 @@
 import AWS from 'aws-sdk';
 import config from './config';
 
-console.log('🤹\tBootstrapping AWS')
+console.log('🤹\tBootstrapping AWS');
 try {
-    AWS.config.update({
-        region: 'eu-central-1',
-        accessKeyId: config.awsAccessKeyId,
-        secretAccessKey: config.awsSecretAccessKey,
-    });
-} catch(e) {
-    console.log('Unable to set AWS, wont use AWS services');
+  AWS.config.update({
+    region: 'eu-central-1',
+    accessKeyId: config.awsAccessKeyId,
+    secretAccessKey: config.awsSecretAccessKey,
+  });
+} catch (e) {
+  console.log('Unable to set AWS, wont use AWS services');
 }
 
 console.log('🏁\tFinished bootstrapping AWS');
