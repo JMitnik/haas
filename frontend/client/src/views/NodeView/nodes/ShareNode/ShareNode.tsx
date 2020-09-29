1c0af31f2348d974071195979ed0e28a3424cc4b
Cold-A-Muse <daan.998@hotmail.com>
2020-09-29 16:36:58 +0200

    Share icon

--- frontend/client/src/views/NodeView/nodes/ShareNode/ShareNode.tsx
+++ frontend/client/src/views/NodeView/nodes/ShareNode/ShareNode.tsx
@@ -1,10 +1,11 @@
+import { Share2 } from 'react-feather';
 import React from 'react';
 
 import { H2, H4 } from '@haas/ui';
 import useDialogueFinish from 'hooks/use-dialogue-finish';
 
 import { GenericNodeProps } from '../types';
-import { ShareNodeContainer } from './ShareNodeStyles';
+import { ShareButton, ShareButtonContainer, ShareNodeContainer } from './ShareNodeStyles';
 
 type SocialShareNodeProps = GenericNodeProps;
 
@@ -12,14 +13,18 @@ const ShareNode = ({ node }: SocialShareNodeProps) => {
   useDialogueFinish();
   console.log('node: ', node);
 
+  const { share } = node;
+
   return (
     <ShareNodeContainer>
-      <H2 color="white">Thank you for participating (ShareNode)!</H2>
-      <H4 color="white" textAlign="center">
-        We will strive towards making you
-        {' '}
-        <i>happier.</i>
-      </H4>
+      <H2 color="white">{node.title}</H2>
+      <ShareButtonContainer>
+        <ShareButton>
+          <Share2 />
+          Share
+        </ShareButton>
+      </ShareButtonContainer>
+
     </ShareNodeContainer>
   );
 };
--- frontend/client/src/views/NodeView/nodes/ShareNode/ShareNodeStyles.tsx
+++ frontend/client/src/views/NodeView/nodes/ShareNode/ShareNodeStyles.tsx
@@ -1,6 +1,31 @@
-import { Div } from '@haas/ui';
-import styled from 'styled-components/macro';
+import Color from 'color';
+
+import { Button, Div, Flex } from '@haas/ui';
+import styled, { css } from 'styled-components/macro';
 
 export const ShareNodeContainer = styled(Div)`
+  display: flex;
+  flex-direction: column;
   justify-content: center;
+`;
+
+export const ShareButtonContainer = styled(Flex)`
+    justify-content: center;
+`;
+
+export const ShareButton = styled(Button)`
+    ${({ theme }) => css`
+        display: flex;
+        justify-content: center;
+        max-width: 200px;
+        border: none;
+        box-shadow: 0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08);
+        border-radius: 10px;
+        padding: 12px 22px;
+        background: ${Color(theme.colors.primary).lighten(0.75).hex()};
+
+        svg {
+            margin-right: 5px;
+        }
+    `}
 `;
