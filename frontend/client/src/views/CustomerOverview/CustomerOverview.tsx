d52f3e39b520cb97bb8c2c6eb35519468bbcc1d9
Jonathan <JMitnik@gmail.com>
2020-10-14 15:19:58 +0200

    Hotfixed customeroverview

--- frontend/client/src/views/CustomerOverview/CustomerOverview.tsx
+++ frontend/client/src/views/CustomerOverview/CustomerOverview.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 
-import { Div, Grid, H1 } from '@haas/ui';
-import CustomerCard from 'components/CustomerCard/CustomerCard';
+import { Div, H1, Text } from '@haas/ui';
+import { motion } from 'framer-motion';
 import Logo from 'components/Logo';
 import useDialogueTree from 'providers/DialogueTreeProvider';
 
 import { CustomerOverviewContainer } from './CustomerOverviewStyles';
 
-const CustomerOverview = ({ customers }: { customers: any }) => {
+const CustomerOverview = () => {
   const store = useDialogueTree();
 
   useEffect(() => {
@@ -18,17 +18,27 @@ const CustomerOverview = ({ customers }: { customers: any }) => {
 
   return (
     <CustomerOverviewContainer exit={{ opacity: 0 }}>
-      <Div useFlex flexDirection="column" minHeight="80vh">
-        <Div useFlex mb="70px" alignItems="flex-end" justifyContent="center">
-          <Logo width={150} />
-          <H1 mb={0} fontSize={[40, 100]} textAlign="center" color="white" ml={4}>
-            haas
-          </H1>
-        </Div>
-
-        <Grid gridTemplateColumns="repeat(auto-fit, minmax(300px, 1fr))" gridGap="24px">
-          {customers?.map((customer: any, index: number) => <CustomerCard key={index} customer={customer} />)}
-        </Grid>
+      <Div alignItems="center" useFlex flexDirection="column" justifyContent="center" minHeight="80vh">
+        <motion.div initial={{ y: 60, opacity: 0 }} animate={{ y: 0, opacity: 1 }}>
+          <Div useFlex mb={2} alignItems="flex-end" justifyContent="center">
+            <Logo width={150} />
+            <H1 mb={0} fontSize={[40, 100]} textAlign="center" color="white" ml={4}>
+              haas
+            </H1>
+          </Div>
+          <motion.div initial={{ y: 30, opacity: 0 }} animate={{ y: 0, opacity: 1, transition: { delay: 0.7 } }}>
+            <Div justifyContent="center" color="white" display="flex">
+              <Text fontSize="1.7rem" mr={1}>
+                happiness
+              </Text>
+              <motion.span initial={{ y: 10, opacity: 0 }} animate={{ y: 0, opacity: 1, transition: { delay: 1.2 } }}>
+                <Text fontSize="1.7rem">
+                  as a service
+                </Text>
+              </motion.span>
+            </Div>
+          </motion.div>
+        </motion.div>
       </Div>
     </CustomerOverviewContainer>
   );
