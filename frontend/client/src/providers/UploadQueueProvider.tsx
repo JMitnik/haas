342d4adef9dffa5ce54f5d6d1dce4016a6a8600c
Jonathan <JMitnik@gmail.com>
2020-10-28 02:07:18 +0100

    Queue cleaned up

--- frontend/client/src/providers/UploadQueueProvider.tsx
+++ frontend/client/src/providers/UploadQueueProvider.tsx
@@ -1,13 +1,8 @@
 import { useMutation } from '@apollo/react-hooks';
-import React, { useCallback, useContext, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useContext } from 'react';
 
-import { autorun } from 'mobx';
-import { observer } from 'mobx-react-lite';
-import { onPatch } from 'mobx-state-tree';
 import createInteractionMutation from 'mutations/createSessionMutation';
 import gql from 'graphql-tag';
-import useDebugMobx from 'hooks/useDebugMobxStore';
-import useDebugReference from 'hooks/useDebugReference';
 import useDialogueTree from 'providers/DialogueTreeProvider';
 
 const UploadQueueContext = React.createContext({} as any);
@@ -22,17 +17,7 @@ const appendToInteractionMutation = gql`
 
 export const UploadQueueProvider = ({ children }: { children: React.ReactNode }) => {
   const store = useDialogueTree();
-
-  const [createInteraction] = useMutation(createInteractionMutation);
-  useDebugReference(createInteraction, 'GraphQL: createInteraction mutation');
-
-  const handleCreateInteraction = () => {
-    // if (entries.length && !hasCreatedSession) {
-    //   createInteraction();
-    // }
-  };
-
-  useDebugMobx(store);
+  const [createInteraction, { data }] = useMutation(createInteractionMutation);
 
   /**
    * Either uploads using the `create` mutation, or appends using the `append` mutation.
@@ -40,26 +25,29 @@ export const UploadQueueProvider = ({ children }: { children: React.ReactNode })
   const handleUploadInteraction = useCallback(() => {
     const uploadEntries = store.relevantSessionEntries;
 
-    createInteraction({
-      variables: {
-        input: {
-          dialogueId: store.tree?.id,
-          entries: uploadEntries.map((entry: any) => ({
-            nodeId: entry.node.node.id,
-            edgeId: entry.edge?.id,
-            depth: entry.depth,
-            data: entry.node?.data,
-          })),
+    // Case 1: We have no uploaded data, yet.
+    if (!data && uploadEntries.length) {
+      createInteraction({
+        variables: {
+          input: {
+            dialogueId: store.tree?.id,
+            entries: uploadEntries.map((entry: any) => ({
+              nodeId: entry.node.node.id,
+              edgeId: entry.edge?.id,
+              depth: entry.depth,
+              data: entry.node?.data,
+            })),
+          },
         },
-      },
-    });
+      });
+    }
   }, [createInteraction, store]);
 
   return (
     <UploadQueueContext.Provider value={{
       isCreatingSession: false,
       uploadInteraction: handleUploadInteraction,
-      createInteraction: handleCreateInteraction,
+      createInteraction: null,
       hasCreatedSession: false,
     }}
     >
