46338941e7ecd4970ab875064c91c8f2354fa973
Cold-A-Muse <daan.998@hotmail.com>
2020-10-28 10:45:57 +0100

    initial commit

--- frontend/dashboard/src/index.tsx
+++ frontend/dashboard/src/index.tsx
@@ -1,3 +1,4 @@
+import { useToast } from '@chakra-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -12,4 +13,22 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+const toast = useToast();
+
+serviceWorker.register({
+  onUpdate: (registration) => {
+    toast({
+      title: 'New version available!',
+      description: 'Closing this popup will reload the page',
+      status: 'info',
+      position: 'top',
+      isClosable: true,
+      onClose: () => {
+        if (registration && registration.waiting) {
+          registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+        }
+        window.location.reload();
+      },
+    });
+  },
+});
