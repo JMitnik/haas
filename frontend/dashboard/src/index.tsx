c8d310d42121873bd2336ee6aae6c1eccd3b8027
Cold-A-Muse <daan.998@hotmail.com>
2020-10-29 09:07:33 +0100

    service worker: new version haas alert

--- frontend/dashboard/src/index.tsx
+++ frontend/dashboard/src/index.tsx
@@ -12,4 +12,12 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register({
+  onUpdate: (registration) => {
+    alert('New version of HAAS available. We are going to update!');
+    if (registration && registration.waiting) {
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+    }
+    window.location.reload();
+  },
+});
