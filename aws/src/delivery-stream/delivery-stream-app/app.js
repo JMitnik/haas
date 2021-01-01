545332fe4064390e4e1d714bbda52af8628b25e9
Jonathan <JMitnik@gmail.com>
2021-01-01 22:45:59 +0100

    feat: added a gateway and first stream with twilio

--- aws/src/delivery-stream/delivery-stream-app/app.js
+++ aws/src/delivery-stream/delivery-stream-app/app.js
@@ -1,10 +1,14 @@
 const AWS = require('aws-sdk');
+const twilio = require('twilio');
 
 const sesClient = new AWS.SES();
-const snsClient = new AWS.SNS();
-const sqsClient = new AWS.SQS();
+let twilioClient;
 
 exports.lambdaHandler = async (event, context, callback) => {
+  const { TWILIO_ACCOUNT_SID, TWILIO_ACCOUNT_AUTH } = await getTwilioSecret();
+
+  twilioClient = twilio(TWILIO_ACCOUNT_SID, TWILIO_ACCOUNT_AUTH);
+
   try {
     await Promise.all(event.Records.map((record) => {
       const row = record.dynamodb.NewImage;
@@ -39,41 +43,93 @@ exports.lambdaHandler = async (event, context, callback) => {
   }
 };
 
-const sendRecordEmail = (
+const sendRecordEmail = async (
   recipient,
   body,
   subject,
   source = 'noreply@haas.live',
 ) => {
-  return sesClient.sendEmail({
-    Destination: {
-      ToAddresses: [recipient],
-    },
-    Message: {
-      Body: {
-        Text: { Data: body },
+  try {
+    await sesClient.sendEmail({
+      Destination: {
+        ToAddresses: [recipient],
       },
+      Message: {
+        Body: {
+          Text: { Data: body },
+        },
 
-      Subject: { Data: subject },
-    },
-    Source: source,
-  }).promise().catch((err) => {
+        Subject: { Data: subject },
+      },
+      Source: source,
+    }).promise();
+  } catch (err) {
     console.error('Error:', err);
-  }).then(() => {
-    console.log('Done!');
-  });
+  }
+  console.log('Done!');
 };
 
-const sendRecordSMS = (
+const sendRecordSMS = async (
   recipient,
   body,
 ) => {
-  return snsClient.publish({
-    PhoneNumber: recipient,
-    Message: body,
-  }).promise().catch((err) => {
-    console.error('Error:', err);
-  }).then(() => {
-    console.log('Done!');
+  try {
+
+  
+  await twilioClient.messages.create({
+    body,
+    to: recipient,
+    // TODO: Put this in a secret manager or something
+    from: '+15027915271'
   });
+    await snsClient.publish({
+      PhoneNumber: recipient,
+      Message: body,
+    }).promise();
+  } catch (err) {
+    console.error('Error:', err);
+  }
+  console.log('Done!');
 };
+
+
+const getTwilioSecret = async () => {
+  let secret;
+  
+  await secretClient.getSecretValue({SecretId: secretName}, function(err, data) {
+      if (err) {
+          if (err.code === 'DecryptionFailureException')
+              // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+              // Deal with the exception here, and/or rethrow at your discretion.
+              throw err;
+          else if (err.code === 'InternalServiceErrorException')
+              // An error occurred on the server side.
+              // Deal with the exception here, and/or rethrow at your discretion.
+              throw err;
+          else if (err.code === 'InvalidParameterException')
+              // You provided an invalid value for a parameter.
+              // Deal with the exception here, and/or rethrow at your discretion.
+              throw err;
+          else if (err.code === 'InvalidRequestException')
+              // You provided a parameter value that is not valid for the current state of the resource.
+              // Deal with the exception here, and/or rethrow at your discretion.
+              throw err;
+          else if (err.code === 'ResourceNotFoundException')
+              // We can't find the resource that you asked for.
+              // Deal with the exception here, and/or rethrow at your discretion.
+              throw err;
+      }
+      else {
+          // Decrypts secret using the associated KMS CMK.
+          // Depending on whether the secret is a string or binary, one of these fields will be populated.
+          if ('SecretString' in data) {
+              secret = data.SecretString;
+          } else {
+              let buff = new Buffer(data.SecretBinary, 'base64');
+              decodedBinarySecret = buff.toString('ascii');
+          }
+      }
+  }).promise();
+  
+  return JSON.parse(secret);
+}
