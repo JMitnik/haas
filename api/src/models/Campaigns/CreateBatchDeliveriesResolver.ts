549509d78faba45c9029ca706666fba0b3963935
Jonathan <JMitnik@gmail.com>
2021-01-01 12:41:35 +0100

    fix: SeS works now

--- api/src/models/Campaigns/CreateBatchDeliveriesResolver.ts
+++ api/src/models/Campaigns/CreateBatchDeliveriesResolver.ts
@@ -218,7 +218,7 @@ export const CreateBatchDeliveriesResolver = mutationField('createBatchDeliverie
           {
             key: 'DeliveryRecipient',
             type: 'string',
-            value: record.email || ''
+            value: (record.variant.campaignVariant.type === 'EMAIL' ? record.email : record.phoneNumber) || ''
           },
           {
             key: 'DeliveryBody',
--- aws/src/delivery-stream/delivery-stream-app/app.js
+++ aws/src/delivery-stream/delivery-stream-app/app.js
@@ -2,48 +2,78 @@ const AWS = require('aws-sdk');
 
 const sesClient = new AWS.SES();
 const snsClient = new AWS.SNS();
+const sqsClient = new AWS.SQS();
 
 exports.lambdaHandler = async (event, context, callback) => {
   try {
-    console.log('Okay I spotted a new entry!');
-    event.Records.forEach((record) => {
-      console.log('Considering record: ', record.dynamodb.NewImage.DeliveryBody.S);
+    await Promise.all(event.Records.map((record) => {
+      const row = record.dynamodb.NewImage;
+      
       if (
         record.eventName === 'MODIFY'
           && record.dynamodb.NewImage.DeliveryStatus.S !== record.dynamodb.OldImage.DeliveryStatus.S
-          && record.dynamodb.NewImage.DeliveryStatus.S === 'DEPLOYED'
-          && record.dynamodb.NewImage.DeliveryRecipient.S
-          && record.dynamodb.NewImage.DeliveryBody.S
+          && row.DeliveryStatus.S === 'DEPLOYED'
+          && row.DeliveryRecipient.S
+          && row.DeliveryBody.S
       ) {
-        console.log('We should mail the following entry:', JSON.stringify(record.dynamodb.NewImage));
+        console.log('We should message the following entry:', JSON.stringify(record.dynamodb.NewImage));
 
-        sesClient.sendEmail({
-          Destination: {
-            ToAddresses: [record.dynamodb.NewImage.DeliveryRecipient.S],
-          },
-          Message: {
-            Body: {
-              Text: { Data: record.dynamodb.NewImage.DeliveryBody.S },
-            },
+        if (row.DeliveryType.S === 'EMAIL') {
+          return sendRecordEmail(
+            row.DeliveryRecipient.S,
+            row.DeliveryBody.S,
+            'A delivery from HAAS!'
+          );
 
-            Subject: { Data: 'Surprise delivery from HAAS!' },
-          },
-          Source: 'noreply@haas.live',
-        }, (err, data) => {
-          if (err) {
-            console.error('Unable to send message. Error JSON:', JSON.stringify(err, null, 2));
-          } else {
-            console.log('Results from sending message: ', JSON.stringify(data, null, 2));
-          }
-        }).promise().catch((err) => {
-          console.error('Error:', err);
-        }).then(() => {
-          console.log('Done!');
-        });
+        } else if (row.DeliveryType.S === 'SMS') {
+          return sendRecordSMS(
+            row.DeliveryRecipient.S,
+            row.DeliveryBody.S
+          );
+        }
       }
-    });
+    }));
   } catch (err) {
     console.log(err);
     callback(null, `Had an error: ${err}`);
   }
+};
+
+const sendRecordEmail = (
+  recipient,
+  body,
+  subject,
+  source = 'noreply@haas.live',
+) => {
+  return sesClient.sendEmail({
+    Destination: {
+      ToAddresses: [recipient],
+    },
+    Message: {
+      Body: {
+        Text: { Data: body },
+      },
+
+      Subject: { Data: subject },
+    },
+    Source: source,
+  }).promise().catch((err) => {
+    console.error('Error:', err);
+  }).then(() => {
+    console.log('Done!');
+  });
+};
+
+const sendRecordSMS = (
+  recipient,
+  body,
+) => {
+  return snsClient.publish({
+    PhoneNumber: recipient,
+    Message: body,
+  }).promise().catch((err) => {
+    console.error('Error:', err);
+  }).then(() => {
+    console.log('Done!');
+  });
 };
