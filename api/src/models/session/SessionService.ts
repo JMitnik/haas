a170ea33c3077e834bcca83fc6d3ed5dd76606a2
Jonathan <JMitnik@gmail.com>
2021-01-07 13:19:30 +0100

    fix: fixed the node types

--- api/src/models/session/SessionService.ts
+++ api/src/models/session/SessionService.ts
@@ -121,7 +121,6 @@ class SessionService {
 
     const textEntries = sessions.flatMap((session) => session.nodeEntries).filter((entry) => {
       const isTextEntry = entry?.relatedNode?.type && TEXT_NODES.includes(entry?.relatedNode?.type);
-
       return isTextEntry;
     });
 
@@ -162,7 +161,7 @@ class SessionService {
     return rootedNodeEntry?.sliderNodeEntry?.value;
   }
 
-  static formatOrderBy(orderByArray?: NexusGenInputs['PaginationSortInput'][]): (SessionOrderByInput|undefined) {
+  static formatOrderBy(orderByArray?: NexusGenInputs['PaginationSortInput'][]): (SessionOrderByInput | undefined) {
     if (!orderByArray?.length) return undefined;
 
     const orderBy = orderByArray[0];
@@ -293,7 +292,7 @@ class SessionService {
       return sessions || [];
     };
 
-    const countSessions = async ({ paginationOpts: paginationOptions, rest } : FindManyCallBackProps) => {
+    const countSessions = async ({ paginationOpts: paginationOptions, rest }: FindManyCallBackProps) => {
       const { dialogueId } = rest;
       const startDate = paginationOptions?.startDate ? new Date(paginationOptions?.startDate) : null;
       const endDate = paginationOptions?.endDate ? new Date(paginationOptions?.endDate) : null;
