afa3035b962ce0c9669c3c0994c62f73d864d9b4
Jonathan <JMitnik@gmail.com>
2021-01-16 15:03:48 +0100

    imp: Docker and stack work

--- aws/stack/bin/main.ts
+++ aws/stack/bin/main.ts
@@ -4,4 +4,4 @@ import * as cdk from '@aws-cdk/core';
 import { MainStack } from '../lib/main-stack';
 
 const app = new cdk.App();
-new MainStack(app, 'TestStack');
+new MainStack(app, 'HaasMainStack');
--- aws/stack/lib/main-stack.ts
+++ aws/stack/lib/main-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
+import { SubnetType } from '@aws-cdk/aws-ec2';
 import * as ecs from '@aws-cdk/aws-ecs';
 import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
+import * as secretsmanager from "@aws-cdk/aws-secretsmanager";
 
-const pathToAPI = '/Users/jonathanmitnik/Developer/haas/code/api/Dockerfile'
+const pathToAPI = '/Users/jonathanmitnik/Developer/haas/code/api'
 
 export class MainStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -11,7 +13,17 @@ export class MainStack extends cdk.Stack {
 
     const vpc = new ec2.Vpc(this, "HAAS_MAIN_VPC", {
       maxAzs: 3,
-      natGateways: 0
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          name: 'HAAS_VPC_PUBLIC_DEFAULT',
+          subnetType: SubnetType.PUBLIC,
+        },
+        {
+          name: 'HAAS_VPC_PRIVATE_DEFAULT',
+          subnetType: SubnetType.ISOLATED,
+        },
+      ],
     });
 
     const cluster = new ecs.Cluster(this, "HAAS_MAIN_CLUSTER", {
@@ -20,12 +32,18 @@ export class MainStack extends cdk.Stack {
       containerInsights: true
     });
 
+    const jwtSecret = secretsmanager.Secret.fromSecretNameV2(this, 'SecretFromName', 'HAAS_JWT');
+
     const fargateApi = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "HAAS_MAIN_API", {
       cluster,
       cpu: 256,
       desiredCount: 1,
+      assignPublicIp: true,
       taskImageOptions: {
-        image: ecs.ContainerImage.fromAsset(),
+        image: ecs.ContainerImage.fromAsset(pathToAPI),
+        environment: {
+          JWT_SECRET: jwtSecret.secretValue.toString()
+        }
       },
     });
   }
