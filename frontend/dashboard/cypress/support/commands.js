ab8c4bbe787e17af97687e14c5ec068c1c5580ce
Jonathan <JMitnik@gmail.com>
2021-03-17 19:09:17 +0100

    imp: whitespace cleanup

--- frontend/dashboard/cypress/support/commands.js
+++ frontend/dashboard/cypress/support/commands.js
@@ -48,7 +48,6 @@ Cypress.Commands.add('login', () => {
 
   cy.visit('http://localhost:3002/verify_token?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNrbTc5ejdmaTAwMDAwMW1oZjhwdzl3a3EiLCJleHAiOjE2MTU4NzI4MzgsImlhdCI6MTYxNTYxMzYzOH0.Zuxda8KPUC7Hw12hcvFgDNtO6AxyZZfiyy0fZ5GhUwc')
 
-
   cy.graphql('refreshAccessToken', (req) => {
     req.reply({ fixture: 'verifyUserToken.json' });
   }, 'refreshAccessToken');
