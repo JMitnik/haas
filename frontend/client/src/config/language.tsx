5e8af6d9e23fae96e9fdb5bfcdbce2573d36ca16
Jonathan <JMitnik@gmail.com>
2021-07-02 10:42:46 +0200

    feat: client translations

--- frontend/client/src/config/language.tsx
+++ frontend/client/src/config/language.tsx
@@ -1,6 +1,16 @@
 /* eslint-disable max-len */
 const language = {
   general: {
+    happy: {
+      en: 'Happy',
+      de: 'Glücklich',
+      nl: 'Tevreden',
+    },
+    unhappy: {
+      en: 'Unhappy',
+      de: 'Unzufrieden',
+      nl: 'Ontevreden',
+    },
     leave_your_details: {
       en: 'Leave your details',
       de: 'Hinterlassen Sie Ihre Daten',
@@ -20,6 +30,21 @@ const language = {
       en: 'Submit',
       de: 'Einreichen',
       nl: 'Verzenden',
+    },
+    quick_release_text: {
+      en: 'That was quick!',
+      de: 'Das war schnell!',
+      nl: 'Dat was snel!',
+    },
+    quick_release_subtext: {
+      en: 'Tap me again if you are sure.',
+      de: 'Tippen Sie erneut auf mich, wenn Sie sicher sind.',
+      nl: 'Druk op mij als je het zeker weet.',
+    },
+    thanks_for_voting: {
+      en: 'Thanks for voting',
+      de: 'Danke für das Abstimmen',
+      nl: 'Bedankt voor het stemmen',
     },
   },
 };
