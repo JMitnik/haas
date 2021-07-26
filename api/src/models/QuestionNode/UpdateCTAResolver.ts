3014ee9bce23364e5d9d59cc5860ac98b45fdf47
Daan <daan.998@hotmail.com>
2021-07-26 13:49:24 +0200

    Cleanup

--- api/src/models/QuestionNode/UpdateCTAResolver.ts
+++ api/src/models/QuestionNode/UpdateCTAResolver.ts
@@ -3,9 +3,7 @@ import { UserInputError } from 'apollo-server-express';
 import { inputObjectType, mutationField } from '@nexus/schema';
 
 import { CTALinksInputType } from '../link/Link';
-import { FormNodeFieldUpsertArgs } from '@prisma/client';
 import { FormNodeInputType, QuestionNodeType, ShareNodeInputType } from '.';
-import { NexusGenInputs } from '../../generated/nexus';
 import { QuestionNodeTypeEnum } from './QuestionNode';
 
 export const UpdateCTAInputType = inputObjectType({
@@ -21,29 +19,6 @@ export const UpdateCTAInputType = inputObjectType({
     t.field('form', { type: FormNodeInputType });
   },
 });
-
-const saveEditFormNodeInput = (input: NexusGenInputs['FormNodeInputType']): FormNodeFieldUpsertArgs[] | undefined => (
-  input.fields?.map((field) => ({
-    create: {
-      type: field.type || 'shortText',
-      label: field.label || '',
-      position: field.position || -1,
-      placeholder: field.placeholder || '',
-      isRequired: field.isRequired || false,
-    },
-    update: {
-      type: field.type || 'shortText',
-      label: field.label || '',
-      position: field.position || -1,
-      placeholder: field.placeholder || '',
-      isRequired: field.isRequired || false,
-    },
-    where: {
-      id: field.id || '-1',
-    },
-  })) || undefined
-);
-
 
 export const UpdateCTAResolver = mutationField('updateCTA', {
   type: QuestionNodeType,
