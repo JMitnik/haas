5d08fd3e7f237ccd32d1cb5bf45f1cd1a8ea8fae
Daan <daan.998@hotmail.com>
2021-07-27 16:13:46 +0200

    Cleanup

--- api/src/models/tag/TagService.ts
+++ api/src/models/tag/TagService.ts
@@ -1,4 +1,5 @@
-import { PrismaClient, Tag, TagCreateInput } from "@prisma/client";
+import { PrismaClient, Tag } from "@prisma/client";
+
 import TagPrismaAdapter from "./TagPrismaAdapter";
 import { CreateTagInput } from "./TagPrismaAdapterType";
 import { CustomerPrismaAdapter } from "../customer/CustomerPrismaAdapter";
@@ -10,10 +11,11 @@ class TagService {
   constructor(prismaClient: PrismaClient) {
     this.tagPrismaAdapter = new TagPrismaAdapter(prismaClient);
     this.customerPrismaAdapter = new CustomerPrismaAdapter(prismaClient);
-  }
+  };
+
   deleteTag(tagId: string): Promise<Tag> {
     return this.tagPrismaAdapter.delete(tagId);
-  }
+  };
 
   async createTag(customerSlug: string, name: string, type: "DEFAULT" | "AGENT" | "LOCATION"): Promise<Tag> {
     const customer = await this.customerPrismaAdapter.findWorkspaceBySlug(customerSlug);
@@ -21,17 +23,19 @@ class TagService {
       name,
       type: type || "DEFAULT",
       customerId: customer?.id || '-1',
-    }
+    };
+
     return this.tagPrismaAdapter.createTag(createTagInput);
-  }
+  };
+
   getAllTagsByCustomerSlug(customerSlug: string): Promise<Tag[]> {
     return this.customerPrismaAdapter.getTagsByCustomerSlug(customerSlug);
-  }
+  };
 
   getAllTagsOfDialogue(dialogueId: string): Promise<Tag[]> {
     return this.tagPrismaAdapter.findTagsByDialogueId(dialogueId);
-  }
+  };
 
-}
+};
 
 export default TagService;
