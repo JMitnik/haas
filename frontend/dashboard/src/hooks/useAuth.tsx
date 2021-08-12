f6d1e7ead6ff1eaf95cdf797787a0a139a530f7b
Jonathan <JMitnik@gmail.com>
2021-08-12 15:21:54 +0200

    fix: added auth + cleaned up stuff

--- frontend/dashboard/src/hooks/useAuth.tsx
+++ frontend/dashboard/src/hooks/useAuth.tsx
@@ -33,55 +33,41 @@ const useAuth = (): UseAuthProps => {
   const { activePermissions } = useCustomer();
 
   const authPermissions = activePermissions || user?.globalPermissions;
+  const isSuperAdmin = user?.globalPermissions?.includes(SystemPermission.CAN_ACCESS_ADMIN_PANEL);
 
-  const hasPermission = (permission: SystemPermission) => authPermissions?.includes(permission) || false;
-  // const hasAllPermissions = (permission: SystemPermission) => authPermissions.includes(permission);
-
-  const canDeleteCustomers = authPermissions?.includes(SystemPermission.CAN_ACCESS_ADMIN_PANEL) || false;
-  const canCreateCustomers = authPermissions?.includes(SystemPermission.CAN_ACCESS_ADMIN_PANEL) || false;
-  const canAccessAdmin = authPermissions?.includes(SystemPermission.CAN_ACCESS_ADMIN_PANEL) || false;
-  const canInviteUsers = authPermissions?.includes(SystemPermission.CAN_ADD_USERS) || false;
-  const canViewUsers = authPermissions?.includes(SystemPermission.CAN_VIEW_USERS) || false;
-  const canDeleteUsers = authPermissions?.includes(SystemPermission.CAN_DELETE_USERS) || false;
-  const canEditUsers = authPermissions?.includes(SystemPermission.CAN_EDIT_USERS) || false;
-
-  const canViewCampaigns = authPermissions?.includes(SystemPermission.CAN_VIEW_CAMPAIGNS) || false;
-  const canCreateCampaigns = authPermissions?.includes(SystemPermission.CAN_CREATE_CAMPAIGNS) || false;
-  const canCreateDeliveries = authPermissions?.includes(SystemPermission.CAN_CREATE_DELIVERIES) || false;
-
-  // Workspace-specific actions
-  const canEditCustomer = authPermissions?.includes(SystemPermission.CAN_EDIT_WORKSPACE) || false;
-  const canViewDialogueBuilder = authPermissions?.includes(SystemPermission.CAN_VIEW_DIALOGUE) || false;
-  const canBuildDialogues = authPermissions?.includes(SystemPermission.CAN_BUILD_DIALOGUE) || false;
-  const canViewDialogueView = authPermissions?.includes(SystemPermission.CAN_VIEW_DIALOGUE_ANALYTICS) || false;
-  const canEditDialogue = authPermissions?.includes(SystemPermission.CAN_EDIT_DIALOGUE) || false;
-  const canDeleteDialogue = authPermissions?.includes(SystemPermission.CAN_DELETE_DIALOGUE) || false;
-
-  const canCreateTriggers = authPermissions?.includes(SystemPermission.CAN_CREATE_TRIGGERS) || false;
-  const canEditTriggers = authPermissions?.includes(SystemPermission.CAN_CREATE_TRIGGERS) || false;
-  const canDeleteTriggers = authPermissions?.includes(SystemPermission.CAN_DELETE_TRIGGERS) || false;
+  /**
+   * Chcek if user has permission to features,
+   * 1. Check if user is SuperAdmin (allow *).
+   * 2. Check if feature is in user's workspace permissions or globalpermissions.
+   * 3. Else, return false.
+   */
+  const hasPermission = (permission: SystemPermission): boolean => (
+    isSuperAdmin
+    || authPermissions?.includes(permission)
+    || false
+  );
 
   return {
-    canDeleteDialogue,
-    canEditDialogue,
-    canViewCampaigns,
-    canCreateCampaigns,
-    canCreateDeliveries,
-    canViewDialogueView,
-    canEditTriggers,
-    canDeleteTriggers,
-    canCreateCustomers,
-    canDeleteCustomers,
-    canCreateTriggers,
-    canEditUsers,
-    canEditCustomer,
-    canInviteUsers,
+    canDeleteDialogue: hasPermission(SystemPermission.CAN_DELETE_DIALOGUE),
+    canEditDialogue: hasPermission(SystemPermission.CAN_EDIT_DIALOGUE),
+    canViewCampaigns: hasPermission(SystemPermission.CAN_VIEW_CAMPAIGNS),
+    canCreateCampaigns: hasPermission(SystemPermission.CAN_CREATE_CAMPAIGNS),
+    canCreateDeliveries: hasPermission(SystemPermission.CAN_CREATE_DELIVERIES),
+    canViewDialogueView: hasPermission(SystemPermission.CAN_VIEW_DIALOGUE_ANALYTICS),
+    canEditTriggers: hasPermission(SystemPermission.CAN_CREATE_TRIGGERS),
+    canDeleteTriggers: hasPermission(SystemPermission.CAN_DELETE_TRIGGERS),
+    canCreateCustomers: hasPermission(SystemPermission.CAN_ACCESS_ADMIN_PANEL),
+    canDeleteCustomers: hasPermission(SystemPermission.CAN_ACCESS_ADMIN_PANEL),
+    canCreateTriggers: hasPermission(SystemPermission.CAN_CREATE_TRIGGERS),
+    canEditUsers: hasPermission(SystemPermission.CAN_EDIT_USERS),
+    canEditCustomer: hasPermission(SystemPermission.CAN_EDIT_WORKSPACE),
+    canInviteUsers: hasPermission(SystemPermission.CAN_ADD_USERS),
+    canAccessAdmin: hasPermission(SystemPermission.CAN_ACCESS_ADMIN_PANEL),
+    canViewUsers: hasPermission(SystemPermission.CAN_VIEW_USERS),
+    canDeleteUsers: hasPermission(SystemPermission.CAN_DELETE_USERS),
+    canViewDialogueBuilder: hasPermission(SystemPermission.CAN_VIEW_DIALOGUE),
+    canBuildDialogues: hasPermission(SystemPermission.CAN_BUILD_DIALOGUE),
     hasPermission,
-    canAccessAdmin,
-    canViewUsers,
-    canDeleteUsers,
-    canViewDialogueBuilder,
-    canBuildDialogues,
   };
 };
 
