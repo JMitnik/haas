2a9aa02add60ce247bc99a0717684d28e7189f84
Daan <daan.998@hotmail.com>
2021-09-30 14:11:17 +0200

    Cleanup (Upsell node client)

--- frontend/client/src/views/NodeView/nodes/SocialShareNode/SocialShareNode.tsx
+++ frontend/client/src/views/NodeView/nodes/SocialShareNode/SocialShareNode.tsx
@@ -1,15 +1,14 @@
-import { Facebook, Globe, Instagram, Linkedin, ShoppingCart, Twitter } from 'react-feather';
-import Color from 'color';
+import { Facebook, Globe, Instagram, Linkedin, Twitter } from 'react-feather';
 import React from 'react';
-import styled, { css } from 'styled-components';
 
-import { Div, Flex } from '@haas/ui';
+import { Flex } from '@haas/ui';
 import { NodeTitle } from 'layouts/NodeLayout/NodeLayoutStyles';
 import { ReactComponent as Whatsapp } from 'assets/icons/icon-whatsapp.svg';
 import CustomIcon from 'components/Icons/CustomIcon';
 
 import { GenericNodeProps } from '../types';
 import { ShareItem, SocialShareNodeContainer } from './SocialShareNodeStyles';
+import UpsellNode from './UpsellNode';
 
 type SocialShareNodeProps = GenericNodeProps;
 
@@ -57,97 +56,6 @@ const DefaultSocialItems = (handleLinkClick: any) => (
   </>
 );
 
-const DrawerContainer = styled(Div)`
-   ${({ theme }) => css`
-  display: grid;
-  row-gap: 2em;
-  background: ${Color(theme.colors.primary).mix(Color('white'), 0.9).saturate(1).hex()};
-  
-  padding: 24px;
-  border-radius: 30px;
-  box-shadow: rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px;
-  backdrop-filter: blur(10px);
-  max-width: 600px;
-  `}
-`;
-
-const TextGradient = styled(Div)`
- ${({ theme }) => css`
-    color: ${Color(theme.colors.primary).isDark() ? Color(theme.colors.primary).hex() : Color(theme.colors.primary).mix(Color('black'), 0.5).saturate(1).hex()};
- `}
-`;
-
-const HeaderContainer = styled(TextGradient)`
-    font-size: 1.5em;
-    font-weight: bold;
-`;
-
-const SubheaderContainer = styled(TextGradient)`
-  font-size: 1em;
-`;
-
-const ImageContainer = styled(Div)`
-  display: flex;
-  justify-content: center;
-
-  img {
-    max-height: 200px;
-    object-fit: contain;
-  }
-`;
-
-const RedirectButton = styled.a`
-   ${({ theme }) => css`
-    display: flex;
-    align-items: center;
-  
-    max-width: fit-content;
-    font-weight: bold;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.05);
-    border-radius: 5px;
-    padding: 12px 32px;
-
-    animation: 2s pulse infinite;
-    text-decoration: none;
-
-    background: linear-gradient(45deg, ${Color(theme.colors.primary).lighten(0.3).hex()}, ${Color(theme.colors.primary).lighten(0.3).saturate(1).hex()}); 
-    font-family: 'Inter', sans-serif;
-    color: ${Color(theme.colors.primary).isDark() ? Color(theme.colors.primary).mix(Color('white'), 0.8).saturate(1).hex() : Color(theme.colors.primary).mix(Color('black'), 0.5).saturate(1).hex()};
-
-    :hover {
-      cursor: pointer;
-      color: ${Color(theme.colors.primary).isDark() ? Color('white').hex() : Color(theme.colors.primary).mix(Color('black'), 0.5).saturate(1).hex()};
-    }
-  `}
-  
-  @keyframes pulse {
-      0% {
-        transform: scale(0.95);
-        box-shadow: 0 0 0 0 rgba(0, 0 ,0, 0.7);
-      }
-      
-      70% {
-        transform: scale(1);
-        box-shadow: 0 0 0 5px rgba(0, 0 ,0, 0);
-      }
-
-      100% {
-        transform: scale(0.95);
-        box-shadow: 0 0 0 0px rgba(0, 0, 0, 0);
-      }
-    }
-`;
-
-const RedirectContainer = styled(Div)`
-  display: flex;
-  justify-content: center;
-  padding-bottom: 1em;
-`;
-
-const IconContainer = styled(Div)`
-  margin-right: 10px;
-`;
-
 const SocialShareNode = ({ node }: SocialShareNodeProps) => {
   // TODO: Use the social capabilities of this event to enqueue
   const handleLinkClick = (event: any) => {
@@ -158,33 +66,7 @@ const SocialShareNode = ({ node }: SocialShareNodeProps) => {
     <SocialShareNodeContainer>
       <NodeTitle>{node.title}</NodeTitle>
       {node.links.length === 1 && (
-        <Flex justifyContent="center">
-          <DrawerContainer>
-
-            <ImageContainer>
-              <img src={node.links[0].imageUrl || ''} alt="product" />
-            </ImageContainer>
-
-            <div style={{ paddingBottom: '1em' }}>
-              <HeaderContainer>
-                {node.links[0].header}
-              </HeaderContainer>
-              <SubheaderContainer>
-                {node.links[0].subHeader}
-              </SubheaderContainer>
-            </div>
-            <RedirectContainer>
-
-              <RedirectButton href={node.links[0].url} target="__blank" rel="noopener noreferrer">
-                <IconContainer>
-                  <ShoppingCart />
-                </IconContainer>
-                {node.links[0].buttonText}
-              </RedirectButton>
-            </RedirectContainer>
-
-          </DrawerContainer>
-        </Flex>
+        <UpsellNode link={node.links[0]} />
       )}
 
       {(node.links.length === 0 || node.links.length > 1) && (
