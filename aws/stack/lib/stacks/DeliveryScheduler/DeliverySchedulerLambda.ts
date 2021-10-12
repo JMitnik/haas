e7de81cae3f34e629f90ecce5878ff35c8bd6c47
Jonathan <JMitnik@gmail.com>
2021-10-12 16:34:59 +0200

    fix

---
+++ aws/stack/lib/stacks/DeliveryScheduler/DeliverySchedulerLambda.ts
@@ -0,0 +1,109 @@
+import * as AWS from 'aws-sdk';
+import { PromiseResult } from 'aws-sdk/lib/request';
+import { format, parse } from 'date-fns';
+
+const credentials = new AWS.SharedIniFileCredentials({ profile: 'haas-prod' });
+
+const dbClient = new AWS.DynamoDB.DocumentClient({
+  region: 'eu-central-1',
+  endpoint: process.env.AWS_SAM_LOCAL ? 'http://host.docker.internal:8000' : undefined,
+  credentials
+});
+
+const TABLE_NAME = 'CampaignDeliveries';
+
+interface Delivery {
+  phoneNumber: string;
+  DeliveryFrom: string;
+  DeliveryRecipient: string;
+  callback: string;
+  DeliveryDate_DeliveryID: string;
+  DeliveryBody: string;
+  DeliveryType: string;
+  DeliveryStatus: string;
+  DeliveryDate: string;
+}
+
+exports.lambdaHandler = async () => {
+  try {
+    const endDate = Date.now();
+    const scheduledItems = await queryPendingScheduledItems(endDate);
+    // const updatedPendingItems = await updatePendingItems(scheduledItems);
+    // return updatedPendingItems;
+
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
+};
+
+/**
+ * Set SCHEDULED items to have a status of DEPLOYED.
+ *
+ * - This will trigger a DynamoDB Stream, which will be handled by another Lambda.
+ * @param items
+ * @returns
+ */
+const updatePendingItems = async (
+  items: AWS.DynamoDB.ItemList
+): Promise<PromiseResult<AWS.DynamoDB.DocumentClient.UpdateItemOutput, AWS.AWSError>[]> => {
+  const updates = items.map((item) => dbClient.update({
+    TableName: TABLE_NAME,
+    Key: {
+      DeliveryDate: item.DeliveryDate,
+      DeliveryDate_DeliveryID: item.DeliveryDate_DeliveryID,
+    },
+    UpdateExpression: 'set DeliveryStatus = :status',
+    ExpressionAttributeValues: {
+      ':status': 'DEPLOYED',
+    },
+  }).promise());
+
+  return await Promise.all(updates);
+}
+
+/**
+ * Query SCHEDULED items which have a delivery-date before NOW.
+ *
+ * - Does this for `maxItems` items. The idea is that
+ */
+const queryPendingScheduledItems = async (
+  endDateTimestamp: Date | number,
+  maxItems: number = 100
+): Promise<AWS.DynamoDB.ItemList> => {
+  const dynamoDate = format(endDateTimestamp, 'ddMMyyyy');
+
+  // Construct query which looks for SCHEDULED items before `now`.
+  const params: AWS.DynamoDB.DocumentClient.QueryInput = {
+    TableName: TABLE_NAME,
+    KeyConditionExpression: 'DeliveryDate = :date AND DeliveryDate_DeliveryID < :now',
+    FilterExpression: 'DeliveryStatus = :deployedStatus',
+    ExpressionAttributeValues: {
+      ':deployedStatus': 'SCHEDULED',
+      ':date': `${dynamoDate}`,
+      ':now': `${new Date(endDateTimestamp).toISOString()}`,
+    },
+  };
+
+  const query = new Promise((resolve, reject) => {
+    dbClient.query(params, ((err, data) => {
+      if (err) {
+        console.error(`An error: ${err}`);
+        reject(err);
+      } else {
+        console.log(`Fetched ${(data.Items || []).length} items!`);
+        resolve(data);
+      }
+    }));
+  });
+
+  const results = await query as AWS.DynamoDB.QueryOutput;
+  const items = results?.Items || [];
+
+  if (items.length > 0) {
+    const numberItems = Math.min(items.length - 1, maxItems);
+    return items.slice(0, numberItems);
+  }
+
+  return items;
+}
