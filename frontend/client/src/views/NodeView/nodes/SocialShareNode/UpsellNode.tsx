4ce3c5c13d4f3bf4af2f911100cb6496ce1af682
Daan <daan.998@hotmail.com>
2021-11-15 16:39:57 +0300

    Cleanup

--- frontend/client/src/views/NodeView/nodes/SocialShareNode/UpsellNode.tsx
+++ frontend/client/src/views/NodeView/nodes/SocialShareNode/UpsellNode.tsx
@@ -66,47 +66,43 @@ const ImageHeader = ({ link }: UpsellNodeProps) => (
   </LS.ImageContainer>
 );
 
-const UpsellNode = ({ link }: UpsellNodeProps) => {
-  const { type } = link;
-  console.log('type: ', type);
-  return (
-    <UI.Flex justifyContent="center">
-      <LS.DrawerContainer>
+const UpsellNode = ({ link }: UpsellNodeProps) => (
+  <UI.Flex justifyContent="center">
+    <LS.DrawerContainer>
 
-        <ImageHeader link={link} />
+      <ImageHeader link={link} />
 
-        <UI.Div pb={2}>
-          <LS.HeaderContainer>
-            {link.header}
-          </LS.HeaderContainer>
-          <LS.SubheaderContainer>
-            {link.subHeader}
-          </LS.SubheaderContainer>
-        </UI.Div>
-        <UI.Flex justifyContent="center" pb={2}>
+      <UI.Div pb={2}>
+        <LS.HeaderContainer>
+          {link.header}
+        </LS.HeaderContainer>
+        <LS.SubheaderContainer>
+          {link.subHeader}
+        </LS.SubheaderContainer>
+      </UI.Div>
+      <UI.Flex justifyContent="center" pb={2}>
 
-          <LS.RedirectButton
-            overrideBackgroundColor={link.backgroundColor || undefined}
-            title={link.title || undefined}
-            href={link.url || undefined}
-            target="__blank"
-            rel="noopener noreferrer"
-          >
-            <UI.Div mr={2}>
-              {link.iconUrl ? (
-                <LS.ButtonIconContainer overrideBackgroundColor={link.backgroundColor || undefined}>
-                  <img alt="Icon on redirect button" src={link.iconUrl} />
-                </LS.ButtonIconContainer>
-              ) : <ShoppingCart />}
+        <LS.RedirectButton
+          overrideBackgroundColor={link.backgroundColor || undefined}
+          title={link.title || undefined}
+          href={link.url || undefined}
+          target="__blank"
+          rel="noopener noreferrer"
+        >
+          <UI.Div mr={2}>
+            {link.iconUrl ? (
+              <LS.ButtonIconContainer overrideBackgroundColor={link.backgroundColor || undefined}>
+                <img alt="Icon on redirect button" src={link.iconUrl} />
+              </LS.ButtonIconContainer>
+            ) : <ShoppingCart />}
 
-            </UI.Div>
-            {link.buttonText || 'Go'}
-          </LS.RedirectButton>
-        </UI.Flex>
+          </UI.Div>
+          {link.buttonText || 'Go'}
+        </LS.RedirectButton>
+      </UI.Flex>
 
-      </LS.DrawerContainer>
-    </UI.Flex>
-  );
-};
+    </LS.DrawerContainer>
+  </UI.Flex>
+);
 
 export default UpsellNode;
