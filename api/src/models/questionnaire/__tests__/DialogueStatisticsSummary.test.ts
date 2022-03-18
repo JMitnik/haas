df0a18219b6f79a519b83d732ce4fd517b238d0b
Daan <daan.998@hotmail.com>
2022-03-18 13:27:55 +0100

    Fix DialogueStatisticsSummary test

--- api/src/models/questionnaire/__tests__/DialogueStatisticsSummary.test.ts
+++ api/src/models/questionnaire/__tests__/DialogueStatisticsSummary.test.ts
@@ -1,3 +1,6 @@
+import { addSeconds, format } from 'date-fns';
+
+import { isValidDateTime } from '../../../utils/isValidDate';
 import { makeTestPrisma } from '../../../test/utils/makeTestPrisma';
 import { clearDialogueDatabase } from './testUtils';
 import { makeTestContext } from '../../../test/utils/makeTestContext';
@@ -15,21 +18,24 @@ describe('DialogueStatisticsSummary', () => {
     await prisma.$disconnect();
   });
 
-  it('can calculate dialogue impact value for all sessions', async () => {
+  it('can calculate impact score for last week', async () => {
     const { user, workspace, dialogue, sliderQuestion, choiceQuestion } = await prepDefaultCreateData(prisma);
-    await seedSession(prisma, dialogue.id, sliderQuestion.id, 70, choiceQuestion.id, 'Facilities');
-    await seedSession(prisma, dialogue.id, sliderQuestion.id, 10, choiceQuestion.id, 'Facilities');
-    await seedSession(prisma, dialogue.id, sliderQuestion.id, 40, choiceQuestion.id, 'Facilities');
+    await seedSession(prisma, dialogue.id, sliderQuestion.id, 70, choiceQuestion.id, 'Facilities', '2022-03-01T06:33:27.899Z');
+    await seedSession(prisma, dialogue.id, sliderQuestion.id, 30, choiceQuestion.id, 'Facilities', '2022-03-02T06:33:27.899Z');
+    await seedSession(prisma, dialogue.id, sliderQuestion.id, 40, choiceQuestion.id, 'Facilities', '2022-03-10T06:33:27.899Z');
 
     // Generate token for API access
     const token = AuthService.createUserToken(user.id, 22);
-    const res = await ctx.client.request(`
+
+    const resStart = await ctx.client.request(`
       query customer {
         customer(slug: "${workspace.slug}") {
           dialogue(where: { slug: "${dialogue.slug}"}) {
             id
             slug
-            dialogueStatisticsSummary {
+            dialogueStatisticsSummary(input: {
+              startDateTime: "01-03-2022"
+              }) {
               nrVotes
               impactScore(type: AVERAGE)
             }
@@ -37,19 +43,20 @@ describe('DialogueStatisticsSummary', () => {
         }
       }
     `,
-      undefined,
+      undefined
+      ,
       {
         'Authorization': `Bearer ${token}`,
       }
     ).then((data) => data?.customer?.dialogue.dialogueStatisticsSummary);
 
-    expect(res).toMatchObject({
-      nrVotes: 3,
-      impactScore: 40,
+    expect(resStart).toMatchObject({
+      nrVotes: 2,
+      impactScore: 50,
     });
   });
 
-  it('can calculate impact score based on scoped sessions', async () => {
+  it('can calculate impact score within 2 days', async () => {
     const { user, workspace, dialogue, sliderQuestion, choiceQuestion } = await prepDefaultCreateData(prisma);
     await seedSession(prisma, dialogue.id, sliderQuestion.id, 70, choiceQuestion.id, 'Facilities', '2022-03-01T06:33:27.899Z');
     await seedSession(prisma, dialogue.id, sliderQuestion.id, 10, choiceQuestion.id, 'Facilities', '2022-03-02T06:33:27.899Z');
@@ -63,9 +70,10 @@ describe('DialogueStatisticsSummary', () => {
           dialogue(where: { slug: "${dialogue.slug}"}) {
             id
             slug
-            dialogueStatisticsSummary(
-              startDateTime: "2022-03-02T06:33:26.899Z",
-              endDateTime: "2022-03-03T06:33:28.899Z") {
+            dialogueStatisticsSummary(input: {
+              startDateTime: "02-03-2022",
+              endDateTime: "03-03-2022"
+            }) {
               nrVotes
               impactScore(type: AVERAGE)
             }
@@ -83,60 +91,6 @@ describe('DialogueStatisticsSummary', () => {
     expect(resWithin).toMatchObject({
       nrVotes: 2,
       impactScore: 25,
-    });
-
-    const resStart = await ctx.client.request(`
-      query customer {
-        customer(slug: "${workspace.slug}") {
-          dialogue(where: { slug: "${dialogue.slug}"}) {
-            id
-            slug
-            dialogueStatisticsSummary(
-              startDateTime: "2022-03-03T06:33:24.899Z") {
-              nrVotes
-              impactScore(type: AVERAGE)
-            }
-          }
-        }
-      }
-    `,
-      undefined
-      ,
-      {
-        'Authorization': `Bearer ${token}`,
-      }
-    ).then((data) => data?.customer?.dialogue.dialogueStatisticsSummary);
-
-    expect(resStart).toMatchObject({
-      nrVotes: 1,
-      impactScore: 40,
-    });
-
-    const resEnd = await ctx.client.request(`
-      query customer {
-        customer(slug: "${workspace.slug}") {
-          dialogue(where: { slug: "${dialogue.slug}"}) {
-            id
-            slug
-            dialogueStatisticsSummary(
-              endDateTime: "2022-03-02T06:33:26.899Z") {
-              nrVotes
-              impactScore(type: AVERAGE)
-            }
-          }
-        }
-      }
-    `,
-      undefined
-      ,
-      {
-        'Authorization': `Bearer ${token}`,
-      }
-    ).then((data) => data?.customer?.dialogue.dialogueStatisticsSummary);
-
-    expect(resEnd).toMatchObject({
-      nrVotes: 1,
-      impactScore: 70,
     });
   });
 
