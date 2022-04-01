e5f996d0f38e86c227a47043e427e0c0b0c00195
Daan <daan.998@hotmail.com>
2022-04-01 13:11:15 +0200

    Fix buildUpdateAutomationTriggerData to match prisma V3 type

--- api/src/models/automations/AutomationPrismaAdapter.ts
+++ api/src/models/automations/AutomationPrismaAdapter.ts
@@ -793,7 +793,7 @@ export class AutomationPrismaAdapter {
             type: action.type,
             apiKey: action.apiKey,
             endpoint: action.endpoint,
-            payload: action.payload as Prisma.InputJsonObject || null,
+            payload: action.payload as Prisma.InputJsonObject || undefined,
           }
         }),
       },
