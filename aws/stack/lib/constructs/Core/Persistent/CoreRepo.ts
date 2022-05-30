167f815655c88bf64d1fd2ae1eaf2403728f6528
Jonathan Mitnik <JMitnik@gmail.com>
2022-05-30 13:40:20 +0200

    imp: prod stack

--- aws/stack/lib/constructs/Core/Persistent/CoreRepo.ts
+++ aws/stack/lib/constructs/Core/Persistent/CoreRepo.ts
@@ -1,18 +1,20 @@
+import { aws_ecr as ecr, aws_ecs_patterns as aws_ecs_patterns } from "aws-cdk-lib";
 import { Construct } from 'constructs';
-import { Stack, aws_ecr as ecr, aws_ecs_patterns as aws_ecs_patterns } from "aws-cdk-lib";
 
 interface CoreRepoProps {
   repoName: string;
 }
 
-export class CoreRepo extends Stack {
+export class CoreRepo extends Construct {
   repo: ecr.Repository;
 
   constructor(scope: Construct, id: string, props: CoreRepoProps) {
     super(scope, id);
 
-    this.repo = new ecr.Repository(this, 'CORE_REPO', {
+    const repo = new ecr.Repository(this, 'CORE_REPO', {
       repositoryName: props.repoName,
     });
+
+    this.repo = repo;
   }
 }
