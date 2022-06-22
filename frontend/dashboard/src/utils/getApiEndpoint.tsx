bd1e198989cec41eae9c471a8dd915e92cfbbe2e
Jonathan Mitnik <JMitnik@gmail.com>
2022-06-22 11:07:03 +0200

    imp: process envify it all

--- frontend/dashboard/src/utils/getApiEndpoint.tsx
+++ frontend/dashboard/src/utils/getApiEndpoint.tsx
@@ -1,1 +1,1 @@
-export const getApiEndpoint = () => import.meta.env.VITE_API_ENDPOINT?.toString() || 'http://localhost:4000/graphql';
+export const getApiEndpoint = () => process.env.VITE_API_ENDPOINT?.toString() || 'http://localhost:4000/graphql';
