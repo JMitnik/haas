c43a4cb1aff41bb901f2643e7681a2d561d46f98
Jonathan Mitnik <JMitnik@gmail.com>
2022-06-22 11:18:09 +0200

    fix: really final fix

--- frontend/dashboard/src/index.tsx
+++ frontend/dashboard/src/index.tsx
@@ -18,7 +18,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-if (import.meta.env.PROD) {
+if (process.env.PROD) {
   serviceWorker.register({
     onUpdate: (registration) => {
       if (confirm('New version of HAAS available. Do you want to update?')) {
