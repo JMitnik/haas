5eb1fd2a7e4a00b8655c6b6bc601ef0b696fde96
Jonathan Mitnik <JMitnik@gmail.com>
2022-06-23 18:30:15 +0200

    feat: translation fix

--- frontend/client/src/modules/GuardModals/IllegalBackModal.tsx
+++ frontend/client/src/modules/GuardModals/IllegalBackModal.tsx
@@ -3,6 +3,7 @@ import { FastForward, Repeat } from 'react-feather';
 import React from 'react';
 
 import * as Modal from 'components/Common/Modal';
+import { useTranslation } from 'react-i18next';
 
 interface IllegalBackModalProps {
   open: boolean;
@@ -10,38 +11,39 @@ interface IllegalBackModalProps {
   onRedo: () => void;
 }
 
-export const IllegalBackModal = ({ open, onRestart, onRedo }: IllegalBackModalProps) => (
-  <Modal.Root open={open} onClose={() => {}}>
-    <UI.ModalBody style={{ minHeight: 'inherit' }}>
-      <UI.H2 color="main.500" textAlign="left">
-        Want to retry?
-      </UI.H2>
-      <UI.Paragraph textAlign="left" color="off.600" style={{ maxWidth: 600 }}>
-        It seems you tried to go back after our last question.
-        <br />
-        We want to ensure that you do not doubly fill in your last answers.
-      </UI.Paragraph>
+export const IllegalBackModal = ({ open, onRestart, onRedo }: IllegalBackModalProps) => {
+  const { t } = useTranslation();
+  return (
+    <Modal.Root open={open} onClose={() => {}}>
+      <UI.ModalBody style={{ minHeight: 'inherit' }}>
+        <UI.H2 color="main.500" textAlign="left">
+          {t('illegal_back_title')}
+        </UI.H2>
+        <UI.Paragraph textAlign="left" color="off.600" style={{ maxWidth: 600, whiteSpace: 'pre-wrap' }}>
+          {t('illegal_back_description')}
+        </UI.Paragraph>
 
-      <UI.Div mt={4}>
-        <UI.Flex flexWrap="wrap">
-          <UI.Button
-            leftIcon={FastForward}
-            variantColor="main"
-            mr={4}
-            mb={[2, 2, 0]}
-            onClick={onRedo}
-          >
-            Back to the last question
-          </UI.Button>
-          <UI.Button
-            leftIcon={Repeat}
-            variantColor="off"
-            onClick={onRestart}
-          >
-            I want to start over
-          </UI.Button>
-        </UI.Flex>
-      </UI.Div>
-    </UI.ModalBody>
-  </Modal.Root>
-);
+        <UI.Div mt={4}>
+          <UI.Flex flexWrap="wrap">
+            <UI.Button
+              leftIcon={FastForward}
+              variantColor="main"
+              mr={4}
+              mb={[2, 2, 0]}
+              onClick={onRedo}
+            >
+              {t('illegal_back_go_back')}
+            </UI.Button>
+            <UI.Button
+              leftIcon={Repeat}
+              variantColor="off"
+              onClick={onRestart}
+            >
+              {t('illegal_back_restart')}
+            </UI.Button>
+          </UI.Flex>
+        </UI.Div>
+      </UI.ModalBody>
+    </Modal.Root>
+  );
+};
