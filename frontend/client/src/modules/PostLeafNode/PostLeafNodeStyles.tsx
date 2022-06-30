2958464a57f3373ae6703ca259719b60d6cfda03
Daan <daan.998@hotmail.com>
2022-06-30 06:48:28 +0200

    Make little text a bit darker so it doesn't fade with background

--- frontend/client/src/modules/PostLeafNode/PostLeafNodeStyles.tsx
+++ frontend/client/src/modules/PostLeafNode/PostLeafNodeStyles.tsx
@@ -5,6 +5,6 @@ import styled, { css } from 'styled-components';
 export const PostLeafNodeContainer = styled(Div)`
   ${({ theme }) => css`
     justify-content: center;
-    color: ${Color(theme.colors.primary).isDark() ? Color(theme.colors.primary).mix(Color('white'), 0.9).saturate(1).hex() : Color(theme.colors.primary).mix(Color('black'), 0.5).saturate(1).hex()};
+    color: ${Color(theme.colors.primary).isDark() ? Color(theme.colors.primary).mix(Color('white'), 0.9).saturate(1).hex() : Color(theme.colors.primary).mix(Color('black'), 0.6).saturate(1).hex()};
   `}
 `;
