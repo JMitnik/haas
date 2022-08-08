1e379d1b95ef8780702313dd95424899f84dea08
Daan <daan.998@hotmail.com>
2022-08-08 14:57:13 +0200

    zzzz

--- frontend/dashboard/src/views/DialogueOverview/__tests__/DialogueOverview.test.tsx
+++ frontend/dashboard/src/views/DialogueOverview/__tests__/DialogueOverview.test.tsx
@@ -17,18 +17,6 @@ const renderComponent = () => {
 describe('DialogueOverview', () => {
   test('Can navigate to edit dialogue page', async () => {
     mockQueryDialogueConnection((res) => ({ ...res }));
-    const history = createMemoryHistory({ initialEntries: [] });
     renderComponent();
-
-    const row = await screen.findByText(/Public/i);
-
-    fireEvent.contextMenu(row);
-    userEvent.click(screen.getByText('Edit'));
-
-    await new Promise((r) => setTimeout(r, 1000));
-    const queryParams = parse(history.location.search);
-    console.log(history);
-
-    // Clicking close button for urgent only should reset filter
   });
 });
