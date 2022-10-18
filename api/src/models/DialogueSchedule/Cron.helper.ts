77f7ccfb97598d03f114a4201d01fc231a61a215
Daan <daan.998@hotmail.com>
2022-10-18 16:58:48 +0800

    Few adjustments

--- api/src/models/DialogueSchedule/Cron.helper.ts
+++ api/src/models/DialogueSchedule/Cron.helper.ts
@@ -1,13 +1,48 @@
 import { GraphQLYogaError } from '@graphql-yoga/node';
 
+export interface SplitCron {
+  minutes: string;
+  hours: string;
+  dayOfMonth: string;
+  month: string;
+  dayOfWeek: string;
+}
+
 export class Cron {
-  constructor(public cron: string) { }
+  public splitCron: SplitCron;
+
+  constructor(public cron: string) {
+    this.splitCron = this.toSplitted();
+  }
+
+  /** Converts an ordinary CRON string into an AWS ready CRON string */
+  public toAWSCronString() {
+    const { minutes, hours, dayOfMonth, dayOfWeek, month } = this.splitCron;
+
+    // Transform the CRON expression to one supported by AWS (? indicator is not part of cron-validator)
+    const scheduledExpression = `cron(${minutes} ${hours} ${dayOfMonth === '*' ? '?' : dayOfMonth} ${month} ${dayOfMonth === '1' ? '?' : dayOfWeek} *)`
+    return scheduledExpression;
+  }
+
+  /**
+   * Converts the dayOfWeek substring (range) from numerical representation of days to text representation
+   * @returns A Cron object but with text representation of days instead of numerical representation
+   */
+  public convertDayOfWeek(): SplitCron {
+    const splitted = this.splitCron.dayOfWeek.split('-');
+    if (splitted.length > 1) {
+      const startDay = this.toDayOfWeek(splitted[0]);
+      const endDay = this.toDayOfWeek(splitted[1]);
+      return { ...this.splitCron, dayOfWeek: `${startDay}-${endDay}` }
+    }
+    return { ...this.splitCron, dayOfWeek: this.toDayOfWeek(this.splitCron.dayOfWeek) };
+  }
 
   /**
    * Splits up a cron string into its separate parts
    * @returns 
    */
-  public toSplitted() {
+  private toSplitted(): SplitCron {
     const splitCron = this.cron.split(' ');
     if (splitCron.length !== 5) throw new GraphQLYogaError('Provided CRON is incorrect format');
 
@@ -16,22 +51,8 @@ export class Cron {
       hours: splitCron[1],
       dayOfMonth: splitCron[2],
       month: splitCron[3],
-      dayOfWeek: this.convertDayOfWeek(splitCron[4]),
+      dayOfWeek: splitCron[4],
     }
-  }
-
-  /**
-   * Converts the dayOfWeek substring (range) from numerical representation of days to text representation
-   * @param dayOfWeek CRON substring
-   */
-  private convertDayOfWeek(dayOfWeek: string) {
-    const splitted = dayOfWeek.split('-');
-    if (splitted.length > 1) {
-      const startDay = this.toDayOfWeek(splitted[0]);
-      const endDay = this.toDayOfWeek(splitted[1]);
-      return `${startDay}-${endDay}`
-    }
-    return this.toDayOfWeek(dayOfWeek);
   }
 
   /**
@@ -60,15 +81,4 @@ export class Cron {
         return dayOfWeek;
     }
   }
-
-  /** Converts an ordinary CRON string into an AWS ready CRON string */
-  toAWSCronString = (): string => {
-    const { minutes, hours, dayOfMonth, dayOfWeek, month } = this.toSplitted();
-
-    // Transform the CRON expression to one supported by AWS (? indicator is not part of cron-validator)
-    const scheduledExpression = `cron(${minutes} ${hours} ${dayOfMonth === '*' ? '?' : dayOfMonth} ${month} ${dayOfMonth === '1' ? '?' : dayOfWeek} *)`
-
-    return scheduledExpression;
-  }
-
 }
